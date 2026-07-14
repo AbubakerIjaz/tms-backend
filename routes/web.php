16bb0c71531d9baadb25247b34eab01932a5ae59
AbubakerIjaz <abubaker@sourcingsquare.com>
2026-07-14 14:13:27 +0500

    add link

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+Route::get('/storage/{path}', function ($path) {
+    $absolutePath = storage_path('app/public/' . $path);
+    if (!File::exists($absolutePath)) { abort(404); }
+    $file = File::get($absolutePath);
+    $type = File::mimeType($absolutePath);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+    $response->header("Cache-Control", "public, max-age=86400");
+    return $response;
+})->where('path', '.*');
